01c899e46f93f69820ba64f41c4e5f81281907df
JLGonzalez033 <71856899+JLGonzalez033@users.noreply.github.com>
2021-02-05 02:33:53 -0700

    Drag end drop y Local Storage

--- Java Script/JS/juego.js
+++ Java Script/JS/juego.js
@@ -2,29 +2,83 @@
 
 var indiceRandom=0;
 var op=[];
+var nievl=1;
+var puntaje=0;
+var elementos=[];
 $(document).ready(function(){
 
-    /*$("#login").animate({
+    $("#login").animate({
         'top':"10%"
-    },300);*/
+    },300);
+    
+    if(localStorage.getItem('puntaje') != null){
+        puntaje = parseInt(localStorage.getItem('puntaje'));
+    }
+    $("#puntaje").text(puntaje);
+    $( "#Elemento" ).droppable({
+        drop: function( event, ui ) {
+          $( this ).addClass( "ui-state-highlight" ).find( "h2" ).html( "Dropped!" );
+         // $(this).css('background-color','green');
+          var i=$(ui.helper[0]).data('numero');
+          console.log(i);
+          if(i == indiceRandom){
+              puntaje++;
+          }else{
+              puntaje--;
+          }
+          $("#puntaje").text(puntaje);
+          localStorage.setItem('puntaje', puntaje);
+          recargar(elementos);
+        },
+        over:function(event, ui){
+            $(this).css('background-color','gray');
+        },
+        over:function(event, ui){
+            $(this).css('background-color','purple');
+        }
+      });
+
     $.getJSON("https://raw.githubusercontent.com/Bowserinator/Periodic-Table-JSON/master/PeriodicTableJSON.json", 
     function(data){
         //console.log(data.elements[0].name);
-        var cuantos = data.elementos.lenght;
-        var random=Math.floor( Math.random() * cuantos);
-        $("Elemento").find("h2").text(data.elements(random).name);
-        for(var i=0;i<3;i++){
-            var int=Math.floor( Math.random() * cuantos);
-            op.push(n);
-            $("#Opciones").find('ul').append('<li><h2>'+ data.elements[n].symbol+'<h2><li>');
-        }
-        $("#Opciones").find('ul').append('<li><h2>'+ data.elements[indiceRandom].symbol+'<h2><li>');
-        op.push(indiceRandom);
-        console.log(op);
+        elementos = data;
+        recargar(elementos)
 
     });
+    function recargar(data){
+        $("#Opciones").find('li').remove();
+        var cuantos = data.elements.lenght;
+        indiceRandom=Math.floor( Math.random() * cuantos);
+        $("#Elemento").find("h2").text( data.elements[indiceRandom].name);
+        for(var i=0;i< (nivel * 3);i++){
+            var n=Math.floor( Math.random() * cuantos);
+            op.push(n);
+            //$("#Opciones").find('ul').append('<li><h2>'+ data.elements[n].symbol+'<h2><li>');
+        }
+        //$("#Opciones").find('ul').append('<li><h2>'+ data.elements[indiceRandom].symbol+'<h2><li>');
+        op.push(indiceRandom);
+
+        op.sort( ()=> Math.random() - 0.5);
+        op.forEach(n => {
+            $("#Opciones").find('ul').append('<li class="drag" data-numero="'+n+'"><h2>'+
+                                    data.elements[n].symbol+'<h2><li>');
+            
+        });
+        $( ".drag" ).draggable({
+            revert:"invalid",
+            refreshPosition:true,
+            copy: true,
+            drag:function(event, ui){
+                $(this).css('opacity','0.5');
+            },
+            stop: function(event, ui){
+                $(this).css('opacity','1');
+            }
+        });
+    }
+
     $("#txtNombre").on('keyup',function(){
-        $("txtNombre").css('border','1px solid white');
+        $("#txtNombre").css('border','1px solid white');
         $(".alerta").fadeout(400);
     })
     $("#btnIniciar").click(function(){
