6e525f733cf38064bb9e8a052aaf338b50645c49
JLGonzalez033 <71856899+JLGonzalez033@users.noreply.github.com>
2021-02-06 12:21:05 -0700

    pagina final
    no se la verdad que tenga

--- Introduccion/admin/php/conexion.php
+++ Introduccion/admin/php/conexion.php
@@ -1,11 +1,11 @@
 <?php
-//base de datos mitiendilla
-//usuario admin
-//password: Z(-+v0{9mbrfs9vw
+//base de datos: id16100066_tiendilla1
+//usuario: id16100066_admin
+//password: KuPE6vY$I~N9b]E-
 $servidor="localhost";
-$nombreBD="mitiendilla";
-$usuario="admin";
-$pass="Z(-+v0{9mbrfs9vw";
+$nombreBD="id16100066_tiendilla1";
+$usuario="id16100066_admin";
+$pass='KuPE6vY$I~N9b]E-';
 $conexion = new mysqli($servidor, $usuario, $pass, $nombreBD);
 if($conexion -> connect_error){
     die("No se pudo conectar al base de datos");  
